7089c7714f82dc9931e6757ec40b952cab12fa4b
lyndon.jonhson.700 <lyndon.jonhson.700@ufrn.edu.br>
2025-07-22 22:35:13 -0300

    fix: validation do RegisterDTO

--- src/main/java/com/projeto/dailytime/domain/user/RegisterDTO.java
+++ src/main/java/com/projeto/dailytime/domain/user/RegisterDTO.java
@@ -1,12 +1,13 @@
 package com.projeto.dailytime.domain.user;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record RegisterDTO(
         @NotBlank(message = "Campo name é obrigatório") String name,
         @NotBlank(message = "Campo email é obrigatório") String email,
         @NotBlank(message = "Campo password é obrigatório") String password,
         @NotBlank(message = "Campo confirmPassword é obrigatório") String confirmPassword,
-        @NotBlank(message = "Campo role é obrigatório") UserRole role
+        @NotNull(message = "Campo role é obrigatório") UserRole role
 ) {
 }
